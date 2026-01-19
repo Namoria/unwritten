06af2bb58f22feaa0663ebaf482213399253c018
Namoria <104687094+Namoria@users.noreply.github.com>
2026-01-19 06:27:20 +0100

    Update proton_vpn_app.sh

--- files/scripts/proton_vpn_app.sh
+++ files/scripts/proton_vpn_app.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 # Install Proton VPN app:
 
 wget "https://repo.protonvpn.com/fedora-$(rpm -E %fedora)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
-dnf5 install -y ./protonvpn-stable-*.rpm && dnf makecache --refresh
+dnf5 install -y ./protonvpn-stable-*.rpm && dnf5 makecache --refresh
 # `proton-vpn-gnome-desktop` will try to run a postcript to enable its systemd service; we will deny this by setting `noscript`,
 # and enable the service manually on the client-side (see README.md):
 dnf5 install -y proton-vpn-gnome-desktop --setopt=tsflags=noscripts
