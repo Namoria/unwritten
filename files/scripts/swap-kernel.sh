fd7f542be77d944ae00e19865979d356d407abc7
Céline <celinek@runbox.com>
2026-09-05 16:50:08 +0200

    clean up files

--- files/scripts/swap-kernel.sh
+++ files/scripts/swap-kernel.sh
@@ -78,7 +78,7 @@ popd >/dev/null
 # Hier nur die Installation – aktiviert (scx_loader.service) und konfiguriert
 # (/etc/scx_loader.toml) wird im Recipe.
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
-dnf5 -y install scx-scheds scx-tools-git
+dnf5 -y install scx-scheds scx-tools
 
 #####################################
 ##  5. Cleanup                     ##
